d4f64ecfc7baf0fb8457a83363a7189b933fff5e
Jane Sandberg <js7389@princeton.edu>
2024-06-06 13:55:40 -0700

    Accept a query parameter
    Co-authored-by: Kevin Reiss <kevinreiss@users.noreply.github.com>
    Co-authored-by: Ryan Laddusaw <rladdusaw@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct QueryParams {
+    query: String,
+}
 
 #[get("/catalog")]
-async fn catalog() -> impl Responder {
-    let body = reqwest::get("https://www.rust-lang.org")
+async fn catalog(query: web::Query<QueryParams>) -> impl Responder {
+    let catalog_url = format!(
+        "https://catalog.princeton.edu/catalog.json?utf8=%E2%9C%93&search_field=all_fields&q={}",
+        query.query
+    );
+    let body = reqwest::get(catalog_url)
     .await.unwrap()
     .text()
     .await.unwrap();
