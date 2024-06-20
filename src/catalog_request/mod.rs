3e334aaa85b219920d5ba65f03664efb9a456606
Christina Chortaria <actspatial@gmail.com>
2024-06-20 16:44:04 -0400

    Test catalog requests Add actix-rt for testing async functions
    Co-authored-by: Jane Sandberg <sandbergja@users.noreply.github.com>
    Co-authored-by: Kevin Reiss <kevinreiss@users.noreply.github.com>
    Co-authored-by: Max Kadel <maxkadel@users.noreply.github.com>
    Co-authored-by: Ryan Laddusaw <rladdusaw@users.noreply.github.com>

--- src/catalog_request/mod.rs
+++ src/catalog_request/mod.rs
@@ -1,4 +1,4 @@
-pub async fn catalog_response_body(query: &String) -> String {
+pub async fn catalog_response_body(query: &str) -> String {
     let catalog_url = format!(
         "https://catalog.princeton.edu/catalog.json?utf8=%E2%9C%93&search_field=all_fields&q={}",
         query
@@ -8,4 +8,14 @@ pub async fn catalog_response_body(query: &String) -> String {
     .text()
     .await.unwrap();
     body
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[actix_rt::test]
+    async fn catalog_response_body_works() {
+        let result= catalog_response_body("cats")
+        .await;
+        assert!(result.contains("cats"));
+    }
 }
